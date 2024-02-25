159abdd5bca29160f08b965807d27ff5e7053953
Javier Fernandez <javi@pccasa.local>
2024-02-25 12:14:27 +0100

    P4: Esquelet

---
+++ src/main/java/practice/examen/ud4/practices/BandurriaHero.java
@@ -0,0 +1,8 @@
+package practice.examen.ud4.practices;
+import java.util.Scanner;
+
+public class BandurriaHero {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+    }
+}
