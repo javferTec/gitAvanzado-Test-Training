6e8abd18f655d602b27dc9e51fc538b117b1ac72
Javier Fernandez <javi@pccasa.local>
2024-02-25 12:23:12 +0100

    P4: Processa entrada

--- src/main/java/practice/examen/ud4/practices/BandurriaHero.java
+++ src/main/java/practice/examen/ud4/practices/BandurriaHero.java
@@ -1,8 +1,14 @@
 package practice.examen.ud4.practices;
+
 import java.util.Scanner;
 
 public class BandurriaHero {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int nombreCasos = in.nextInt();
+
+        for (int i = 0; i < nombreCasos; i++) {
+            String liniaBandurria = in.nextLine();
+        }
     }
 }
