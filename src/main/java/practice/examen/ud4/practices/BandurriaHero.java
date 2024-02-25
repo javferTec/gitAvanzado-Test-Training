e88226070e7ed9df679d7b771738cfe160bc9f96
Javier Fernandez <javi@pccasa.local>
2024-02-25 12:31:25 +0100

    P4: Processa els casos d'entrada:

--- src/main/java/practice/examen/ud4/practices/BandurriaHero.java
+++ src/main/java/practice/examen/ud4/practices/BandurriaHero.java
@@ -23,6 +23,7 @@ public class BandurriaHero {
 
         for (int i = 0; i < nombreCasos; i++) {
             String liniaBandurria = in.nextLine();
+            System.out.println(puntuacio(liniaBandurria));
         }
     }
 }
