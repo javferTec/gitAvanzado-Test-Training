a78d2f05c053c0524cd0c4f9932fcabfcce61842
Javier Fernandez <javi@pccasa.local>
2024-02-25 12:21:08 +0100

    Revert "P4: Definició mètode puntuacio()"
    This reverts commit 04bd4bb0a003e785759a63e6f7ab9ddc52086eaa.

--- src/main/java/practice/examen/ud4/practices/BandurriaHero.java
+++ src/main/java/practice/examen/ud4/practices/BandurriaHero.java
@@ -2,9 +2,6 @@ package practice.examen.ud4.practices;
 import java.util.Scanner;
 
 public class BandurriaHero {
-    public static int puntuacio(String notesCorrectes){
-            return 0;
-        }
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
     }
